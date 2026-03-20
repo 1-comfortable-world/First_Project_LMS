332e9a3647d66bade04e103ee657a3597abd9d03
kohs1195123-del <kohs1195123@gmail.com>
2026-03-20 09:46:51 +0900

    application tnwjd2

---
+++ src/main/java/com/wanted/only_one/Application.java
@@ -0,0 +1,39 @@
+package com.wanted.only_one;
+
+import com.wanted.only_one.global.config.JDBCTemplate;
+import com.wanted.only_one.member.controller.AuthController;
+import com.wanted.only_one.member.controller.MemberController;
+import com.wanted.only_one.member.service.AuthService;
+import com.wanted.only_one.member.service.MemberService;
+import com.wanted.only_one.member.view.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Application {
+    public static void main(String[] args) {
+
+        try (Connection con = JDBCTemplate.getConnection()) {
+            System.out.println("✅데이터베이스 연결 성공");
+            JDBCTemplate.printConnectionStatus();
+
+            AuthService authService = new AuthService(con);
+            MemberService memberService = new MemberService(con);
+
+
+            AuthController authController = new AuthController(authService);
+            MemberController memberController = new MemberController(memberService);
+
+            MemberOutputView outputView = new MemberOutputView();
+
+            MemberInputView InputView= new MemberInputView(authController, outputView);
+
+            InputView.displayMainMenu();
+
+        } catch (SQLException e) {
+            System.err.println("🚨데이터베이스 연결 실패");
+        }finally{
+            JDBCTemplate.close();
+        }
+    }
+}
